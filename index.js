06e45c34b67fc3c4ffb11fae3115e509d11b8ed3
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-05-04 23:44:31 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+//Code your solution here
+function findMatching(array, b) {
+   return array.filter(x => x.toUpperCase() === b.toUpperCase());
+} 
+
+function fuzzyMatch(array, s) {
+    return array.filter(x => x.toUpperCase()[0] === s.toUpperCase()[0]);
+        }
+
+function matchName(array) {
+    return(array.filter(x => x.name === "Bobby"))
+}
